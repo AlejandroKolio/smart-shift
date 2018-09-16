04cd5d41a9c843ca193e88594283a9dadb888c46
Alexander Shakhov <write.me.messages@gmail.com>
2018-09-16 12:50:13 +0300

    initial commit

---
+++ src/test/java/com/scheduler/app/smartshift/SmartShiftApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scheduler.app.smartshift;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartShiftApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
